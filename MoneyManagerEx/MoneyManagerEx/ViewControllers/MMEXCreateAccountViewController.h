d2d7edbf24d8ec0e34880e32e1d4b81c0bdc96d4
taoyuxuan <yunmengzhidian@126.com>
2014-10-20 00:09:45 +0800

    Initial UI(one part)

---
+++ MoneyManagerEx/MoneyManagerEx/ViewControllers/MMEXCreateAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMEXCreateAccountViewController.h
+//  MoneyManagerEx
+//
+//  Created by taotao on 14/10/19.
+//  Copyright (c) 2014年 taotao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMEXCreateAccountViewController : UIViewController
+
+@end
