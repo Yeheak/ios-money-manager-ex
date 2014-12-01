c63f4d11278e3b044fcaf941c4860d3bee2044fa
taoyuxuan <yunmengzhidian@126.com>
2014-12-01 23:16:21 +0800

    add new account page layout and content

---
+++ MoneyManagerEx/ViewControllers/MMEXCreateAccountViewController.h
@@ -0,0 +1,33 @@
+//
+//  MMEXCreateAccountViewController.h
+//  MoneyManagerEx
+//
+//  Created by taoyuxuan on 14/10/19.
+//  Copyright (c) 2014 taoyuxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TypeDef.h"
+
+@interface MMEXCreateAccountViewController : UITableViewController
+
+@property (nonatomic, strong) IBOutlet UITextField  *accountName;
+@property (nonatomic, strong) IBOutlet UILabel      *accountType;
+@property (nonatomic, strong) IBOutlet UILabel      *currencyType;
+@property (nonatomic, strong) IBOutlet UITextField  *initialMoney;
+@property (nonatomic, strong) IBOutlet UILabel      *accountStatus;
+@property (nonatomic, strong) IBOutlet UITextField  *accountNum;
+@property (nonatomic, strong) IBOutlet UITextField  *merchat;
+@property (nonatomic, strong) IBOutlet UITextField  *merchatWeb;
+@property (nonatomic, strong) IBOutlet UITextField  *merchatContact;
+@property (nonatomic, strong) IBOutlet UITextField  *loginInfo;
+@property (nonatomic, strong) IBOutlet UITextField  *note;
+
+// value
+@property (nonatomic, assign) EnumAccountType        accountTypeEnumValue;
+@property (nonatomic, assign) EnumAccountStatus      accountStatusEnumValue;
+@property (nonatomic, assign) NSInteger              currencyTypeValue;
+
+- (IBAction)resignTheKeyBoard:(id)sender;
+
+@end
